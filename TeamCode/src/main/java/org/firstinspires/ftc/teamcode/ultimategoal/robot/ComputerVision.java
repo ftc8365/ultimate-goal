072f9d0e3aaf7786266b03665e92b3d7d174d2cd
RyanA <Ryanalli77@gmail.com>
2020-09-26 15:18:34 -0400

    Ryan A commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/robot/ComputerVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/robot/ComputerVision.java
@@ -21,7 +21,12 @@ public class ComputerVision {
             "AaaD61H/////AAABmfJ7OgkkWEWVmniO8RAqZ1cEkXF6bR9ebw4Gw+hUI8s1s5iTA9Hyri+sjoSO/ISwSWxfZCI/iAzZ0RxSQyGQ7xjWaoE4AJgn4pKLKVcOsuglHJQhjQevzdFKWX6cXq4xYL6vzwX7G7zuUP6Iw3/TzZIAj7OxYl49mA30JfoXvq/kKnhDOdM531dbRyZiaNwTGibRl5Dxd4urQ5av3EU1QyFBWR04eKWBrJGffk8bdqjAbB3QDp/7ZAMi2WfkItMOP5ghc5arNRCNt5x+xX7gSq8pMt3ZoC3XPfRNNaEbf24MgaNJXlUARsfAuycgPiY83jbX0Hrctj4wZ20wqah+FNYMqvySokw6/fDmyG0mPmel";
 
     // TODO : Declare Vurforia & TensorFlow Variables
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
 
+    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Quad";
+    private static final String LABEL_SECOND_ELEMENT = "Single";
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     public ComputerVision(OpMode opMode) {
@@ -32,26 +37,68 @@ public class ComputerVision {
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     // TODO
     public void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     // TODO
     public void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     // TODO
     public void activate() {
+        if (tfod != null) {
+            tfod.activate();
+        }
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     // TODO
     public void detect() {
+        if (tfod != null) {
+            // getUpdatedRecognitions() will return null if no new information is available since
+            // the last time that call was made.
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                telemetry.addData("# Object Detected", updatedRecognitions.size());
+                // step through the list of recognitions and display boundary info.
+                int i = 0;
+                for (Recognition recognition : updatedRecognitions) {
+                    telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                    telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                            recognition.getLeft(), recognition.getTop());
+                    telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                            recognition.getRight(), recognition.getBottom());
+                }
+                telemetry.update();
+            }
+        }
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     // TODO
     public void shutdown() {
+        if (tfod != null) {
+            tfod.shutdown();
+        }
     }
 
 }
