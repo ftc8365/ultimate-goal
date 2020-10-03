f355a02d4bb588f67ac6dcc90c3b8ee19187b156
ken <ngaik999@gmail.com>
2020-10-03 11:46:40 -0400

    Bulk read

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/opmode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/opmode/DriverControl.java
@@ -6,31 +6,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.ultimategoal.robot.*;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name="DriverControl", group="TeleOp")
 //@Disabled
 
 public class DriverControl extends LinearOpMode {
     Robot robot = new Robot(this);
 
-//    double DRIVE_NORMAL_POWER_RATIO = 1.00;
-    double DRIVE_NORMAL_POWER_RATIO = 0.30;
+    double DRIVE_NORMAL_POWER_RATIO = 1.00;
+//    double DRIVE_NORMAL_POWER_RATIO = 0.30;
     double DRIVE_LOW_POWER_RATIO    = 0.30;
-//    double TURN_NORMAL_POWER_RATIO  = 0.75;
-    double TURN_NORMAL_POWER_RATIO  = 0.35;
+    double TURN_NORMAL_POWER_RATIO  = 0.75;
+//    double TURN_NORMAL_POWER_RATIO  = 0.35;
     double TURN_LOW_POWER_RATIO     = 0.25;
 
     boolean driveNormalMode = true;
